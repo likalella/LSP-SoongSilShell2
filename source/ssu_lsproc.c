842406743a039cc54eca177cdbc46472083bdf01
likalella <likalella95@gmail.com>
2017-04-20 04:37:26 -0700

    add ssu_lsproc.c, ssu_lsproc.h

---
+++ source/ssu_lsproc.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ssu_lsproc.h"
+
+int ssu_lsproc(int argc, char* argv[]){
+	printf("ssu_lsproc()\n");
+}
---
+++ source/ssu_lsproc.h
@@ -0,0 +1,5 @@
+#ifndef SSU_LSPROC_H
+#define SSU_LSPROC_H
+
+int ssu_lsproc(int argc, char *argv[]);
+#endif
