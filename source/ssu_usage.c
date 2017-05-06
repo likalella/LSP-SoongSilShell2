8d048ed7aaf6487d9c0daa7ab1b14ba110c1ce41
lika <likalella95@gmail.com>
2017-05-06 05:31:45 -0700

    add usage mesg

--- source/ssu_usage.c
+++ source/ssu_usage.c
@@ -4,5 +4,18 @@
 #include "ssu_usage.h"
 
 void lsprocUsage(void){
-	printf("Usage : lsproce\n");
+	printf("Usage: ssu_lsproc [OPTION]\n");
+	printf("List information about process and systems in proc file system.\n");
+	printf("  -f [PID1] [PID2]...[PID16]  :\n");
+	printf("  -t [PID1] [PID2]...[PID16]  :\n");
+	printf("  -c [PID1] [PID2]...[PID16]  :\n");
+	printf("  -n [PID1] [PID2]...[PID16]  :\n");
+	printf("  -m [PID1] [PID2]...[PID16] [-k] [KEY1] [KEY2]...[KEY16]\n");
+	printf("  -w                          :\n");
+	printf("  -e                          :\n");
+	printf("  -l                          :\n");
+	printf("  -v                          :\n");
+	printf("  -r                          :\n");
+	printf("  -s [ATTRIBUTE_NAME]         :\n");
+	printf("  -o [FILE_NAME]              :\n");
 }
